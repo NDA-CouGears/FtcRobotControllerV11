44e3b0bcb4630dda9fbbae3a93b8cca419838a9b
Bill Clogston <bill@clogstons.com>
2025-11-23 18:49:19 -0500

    Added comments identifying some issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrepareLaunch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrepareLaunch.java
@@ -10,6 +10,9 @@ public class PrepareLaunch extends CarouselOperations{
     private float launchB3 = zero + (2*CTR/3); */
 
     public PrepareLaunch(int bay){
+        // Bill: This needs to move into loop after we update curPos, offset and zero since they
+        // change throughout the run and may be different by the time this operation is run
+        // We may also want to move the math into a constant instead of redoing it each time
         if (bay == 1){
             targetPos = zero;
         }
@@ -23,6 +26,9 @@ public class PrepareLaunch extends CarouselOperations{
 
     @Override
     public void loop() {
+        // Bill: We need to move the update of offset and zero to after we update curPos since they
+        // depend on it. We could move the assignment of curPos to the parent loop or call
+        // super.loop() below the assignment here, but I think it make more sense int he parent
         super.loop();
         robot.carousel.setTargetPosition((int)(targetPos+.5));
         robot.carousel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrepareLoad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrepareLoad.java
@@ -6,6 +6,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 public class PrepareLoad extends CarouselOperations{
     public PrepareLoad(int bay){
+        // Bill: This needs to move into loop after we update curPos, offset and zero since they
+        // change throughout the run and may be different by the time this operation is run
+        // We may also want to move the math into a constant instead of redoing it each time
         if (bay == 1){
             targetPos = zero + CTR/2;
         }
@@ -19,6 +22,9 @@ public class PrepareLoad extends CarouselOperations{
 
     @Override
     public void loop() {
+        // Bill: We need to move the update of offset and zero to after we update curPos since they
+        // depend on it. We could move the assignment of curPos to the parent loop or call
+        // super.loop() below the assignment here, but I think it make more sense int he parent
         super.loop();
         robot.carousel.setTargetPosition((int)(targetPos+.5));
         robot.carousel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
