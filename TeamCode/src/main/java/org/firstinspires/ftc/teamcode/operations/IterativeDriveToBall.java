0f49ceb8009eed336c7ef22b7b79e5e4e2d7f4be
Cougears-NDA <cougears@ndaHingham.com>
2026-05-13 17:02:01 -0400

    distance broke it...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/operations/IterativeDriveToBall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/operations/IterativeDriveToBall.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.operations;
 
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.IterativeRobotParent;
+
 public class IterativeDriveToBall extends RobotOperation {
     boolean finished = false;
     @Override
@@ -13,12 +15,23 @@ public class IterativeDriveToBall extends RobotOperation {
         double headingError = errors[1];
 
         double turnSpeed = Range.clip(headingError * .02, -1, 1);
+        double ySpeed = 0;
+        if (distance > 0.1) {
+            ySpeed = Range.clip(distance * IterativeRobotParent.P_DRIVE_GAIN, 0.1, 1);
+        }
+
         if (Math.abs(headingError) > 1) {
             robot.moveRobot(0,0,turnSpeed);
         }
+        /* else if (Math.abs(distance) > 24) {
+            robot.moveRobot(0,ySpeed,0);
+        }
+         */
         else {
             robot.moveRobot(0,0,0);
+            //finished = true;
         }
+
     }
 
     @Override
